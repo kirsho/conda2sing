d3e9f7a231ae7593f064a2685e98d408f27796ac
Olivier Kirsh <OlivierKirsh@univ-paris-diderot.fr>
2019-08-29 21:23:08 +0200

    V1 script conda2sing

---
+++ condacreate2sing.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# script to create a conda env with apps in a singularity image 
+# 1st argument is image name
+# 2nd argument is env name
+# 3rd arguments are app names. if more than one must be between quote
+# and export env config in a yml file
+
+SIMG=$1
+DEFNAME=$2
+APPS=$3
+
+echo 'Bootstrap: docker
+From: continuumio/miniconda3
+
+# This file is a singularity definition file to create simg with conda
+# It starts with a docker image of miniconda continuumio/miniconda3
+# It allows direct creation of the env by specifying which package (or with a .yml file)
+# unlock desired method by removing # and set variable (defname) or file name (xxxx.yml) 
+# future dev: if else, yml or package names as argument for $ singularity build command
+# Image build with ($ sudo singularity build imagename.simg Singularity )
+
+
+%labels
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Maintainer Olivier Kirsh <olivier.kirsh@u-paris.fr>					
+    Version v1.3-2 20190829
+
+%files
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# load the definition files
+
+	Singularity							## Definition file (keep this name to allow shub cloud build)
+
+%environment
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Set global environment variables for anything run within the container
+
+	defname=DEFNAME							## Set environment name
+	PATH=/opt/conda/envs/$defname/bin:$PATH				## Put the environment in the PATH (no $ conda activate xx required)
+
+%post
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# What is executed during the build process
+
+# Edit .bashrc to run conda    	
+	echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc		## Enable conda for the current user
+									## Better than $ echo "conda activate" >> ~/.bashrc
+									## or $ export PATH="/opt/miniconda3/bin:$PATH" (not recommanded)
+
+# Set conda channels 
+	/opt/conda/bin/conda config --add channels defaults
+	/opt/conda/bin/conda config --add channels bioconda
+	/opt/conda/bin/conda config --add channels conda-forge
+
+# Update conda
+	#/opt/conda/bin/conda update -n base conda  			## Optionnal. Or specify version
+
+
+# Conda install
+	defname=DEFNAME 						## Set environment name
+	/opt/conda/bin/conda create -n $defname APPS			## package name or python version. 
+	/opt/conda/bin/conda clean --tarballs				## Clean and light weight env
+	mkdir -p /setupfile						## Create /setupfile directory to save and trace env recipe
+	mv Singularity /setupfile
+	cd /setupfile
+	/opt/conda/bin/conda list -n $defname > $defname_installed_packages.md
+	/opt/conda/bin/conda env export --no-build -n $defname > $defname.yml
+	
+
+%runscript
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This executes commands
+    	exec "$@"
+
+' > Singularity
+
+
+sed -i -e "s/DEFNAME/$2/g" Singularity
+
+sed -i -e "s/APPS/$3/g" Singularity
+
+
+
+sudo singularity build $1.simg Singularity
+
+
